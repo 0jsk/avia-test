82ac76362c21b0539e3e7bd24ae77db6e6c47fa9
0jsk <90405410+0jsk@users.noreply.github.com>
2022-01-26 22:39:46 +0300

    feat(api, tickets): add query selector for tickets

--- pages/api/tickets.ts
+++ pages/api/tickets.ts
@@ -5,6 +5,8 @@ import ticketsData from '@/data/tickets.json';
 const tickets: Ticket[] = ticketsData;
 
 // eslint-disable-next-line require-await
-export default (async (_request, response) => {
-  response.json(tickets);
+export default (async (request, response) => {
+  const { start = 0, end = 5 } = request.query;
+
+  response.json(tickets.slice(+start, +end));
 }) as NextApiHandler;
