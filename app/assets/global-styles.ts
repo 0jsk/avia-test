9aa08977d17c5e077eea8a1345922bb8082915bc
0jsk <90405410+0jsk@users.noreply.github.com>
2022-01-27 01:04:48 +0300

    feat(shared, ui): add Text component

--- app/assets/global-styles.ts
+++ app/assets/global-styles.ts
@@ -7,8 +7,6 @@ export const GlobalStyles = createGlobalStyle`
 
     margin: 0;
     padding: 0;
-
-    font-family: 'Open Sans', sans-serif;
   }
 
   html,
---
+++ app/shared/ui/atoms/text.tsx
@@ -0,0 +1,65 @@
+import type { DefaultTheme } from 'styled-components';
+import type { FC } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import { theme } from '@app/assets';
+
+type Props = {
+  type?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span';
+  className?: string;
+  color?: string & DefaultTheme['color']['text'];
+  title?: string;
+};
+
+const TextStyled = styled.p<{ 'data-type': Props['type']; color: Props['color'] }>`
+  font-family: 'Open Sans', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  color: ${props => theme.color.text?.[props.color] ?? theme.color.text.primary}
+
+  &[data-type='p'] {
+    font-size: 13px;
+    line-height: 20px;
+  }
+
+  &[data-type='span'] {
+    font-size: 12px;
+    line-height: 20px;
+  }
+
+  &[data-type='h1'] {
+    font-size: 32px;
+    font-weight: 600;
+  }
+
+  &[data-type='h2'] {
+    font-size: 28px;
+    font-weight: 600;
+  }
+
+  &[data-type='h3'] {
+    font-size: 24px;
+    font-weight: 600;
+  }
+
+  &[data-type='h4'] {
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  &[data-type='h5'] {
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  &[data-type='h6'] {
+    font-size: 18px;
+    font-weight: 600;
+  }
+`;
+
+export const Text: FC<Props> = ({ children, type, className, color, title }) => (
+  <TextStyled data-type={type} as={type} className={className} color={color} title={title}>
+    {children}
+  </TextStyled>
+);
