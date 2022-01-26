f16f5fb188390eacfba005e24879921619dec885
0jsk <90405410+0jsk@users.noreply.github.com>
2022-01-27 00:05:18 +0300

    feat(company, api): add companies list fetch

---
+++ app/modules/Company/api.ts
@@ -0,0 +1,12 @@
+import type { Company } from '@app/modules/Company';
+import { CompanyC } from '@app/modules/Company';
+import { array } from 'io-ts';
+import { assert } from '@app/utils/io-ts';
+
+export const fetchCompanies = (): Promise<Company[]> => fetch(
+  '/api/companies',
+  { method: 'GET' }
+)
+  .then(res => res.json())
+  .then(array(CompanyC).decode)
+  .then(assert);
